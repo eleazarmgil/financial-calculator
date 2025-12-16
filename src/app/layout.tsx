9c044a354fc5641ca08c13d738ba955f3037e90c
eleazarmgil <maug.lucas@gmail.com>
2025-12-16 08:51:34 -0400

    refactor: move project structure to src directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+    weight: ['300', '400', '500', '600', '700'],
+    subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
     title: "Financial Calculator - Análisis de Ingresos y Costos",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="es">
-            <body className={inter.className}>{children}</body>
+            <body className={poppins.className}>{children}</body>
         </html>
     );
 }
